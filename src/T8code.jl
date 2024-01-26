cdf9af9aa7508abc5fd53f13f1fef815fbb7d8ae
Johannes Markert <johannes.markert@dlr.de>
2024-01-26 17:12:47 +0100

    Added exports.

--- src/T8code.jl
+++ src/T8code.jl
@@ -210,6 +210,7 @@ end
 # needed nevertheless by some application codes. This will be fixed resp. more
 # streamlined in future releases of t8code.
 
+export t8_forest_ghost_get_remotes
 function t8_forest_ghost_get_remotes(forest)
     num_remotes_ref = Ref{Cint}()
     remotes_ptr = @ccall T8code.Libt8.libt8.t8_forest_ghost_get_remotes(forest::t8_forest_t,
@@ -217,20 +218,24 @@ function t8_forest_ghost_get_remotes(forest)
     remotes = unsafe_wrap(Array, remotes_ptr, num_remotes_ref[])
 end
 
+export t8_forest_ghost_remote_first_elem
 function t8_forest_ghost_remote_first_elem(forest, remote)
     @ccall T8code.Libt8.libt8.t8_forest_ghost_remote_first_elem(forest::t8_forest_t,
                                                                 remote::Cint)::t8_locidx_t
 end
 
+export t8_forest_ghost_num_trees
 function t8_forest_ghost_num_trees(forest)
     @ccall T8code.Libt8.libt8.t8_forest_ghost_num_trees(forest::t8_forest_t)::t8_locidx_t
 end
 
+export t8_forest_ghost_get_tree_element_offset
 function t8_forest_ghost_get_tree_element_offset(forest, lghost_tree)
     @ccall T8code.Libt8.libt8.t8_forest_ghost_get_tree_element_offset(forest::t8_forest_t,
                                                                       lghost_tree::t8_locidx_t)::t8_locidx_t
 end
 
+export t8_forest_ghost_get_global_treeid
 function t8_forest_ghost_get_global_treeid(forest, lghost_tree)
     @ccall T8code.Libt8.libt8.t8_forest_ghost_get_global_treeid(forest::t8_forest_t,
                                                                 lghost_tree::t8_locidx_t)::t8_gloidx_t

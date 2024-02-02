02f40d33347d657cd964585aa6ed9fb08c856e3c
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2024-02-02 11:58:34 +0100

    format (#54)

--- test/test_all.jl
+++ test/test_all.jl
@@ -13,22 +13,22 @@ MPI.Init()
 comm = MPI.COMM_WORLD
 
 @testset "init" begin
-  include("test_init.jl")
+    include("test_init.jl")
 end
 
 @testset "general" begin
-  if !Sys.iswindows()
-    # These tests do not work in Windows since the DLL loader does not search for symbols beyond libt8.dll.
-    include("test_refcount.jl")
-  end
+    if !Sys.iswindows()
+        # These tests do not work in Windows since the DLL loader does not search for symbols beyond libt8.dll.
+        include("test_refcount.jl")
+    end
 end
 
 @testset "cmesh" begin
-  include("cmesh/test_readmshfile.jl")
+    include("cmesh/test_readmshfile.jl")
 end
 
 @testset "forest" begin
-  include("forest/test_element_volume.jl")
+    include("forest/test_element_volume.jl")
 end
 
 end # module

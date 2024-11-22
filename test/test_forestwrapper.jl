f1f1e7b4bcb193909c1483704eed4a63584f5ed3
Johannes Markert <johannes.markert@dlr.de>
2024-11-22 16:18:58 +0100

    Added test evals.

--- test/test_forestwrapper.jl
+++ test/test_forestwrapper.jl
@@ -9,11 +9,15 @@
         @test status == 0
     end
 
+    @test length(T8code.T8CODE_OBJECT_TRACKER) == 0
+
     # Create a forest and wrap by `ForestWrapper`
     scheme = t8_scheme_new_default_cxx()
     cmesh = t8_cmesh_new_hypercube(T8_ECLASS_QUAD, comm, 0, 0, 0)
     forest = t8_forest_new_uniform(cmesh, scheme, 0, 0, comm)
     wrapper_A = T8code.ForestWrapper(forest)
+
+    @test length(T8code.T8CODE_OBJECT_TRACKER) == 1
 
     # Create another forest and wrap by `ForestWrapper`
     scheme = t8_scheme_new_default_cxx()
@@ -21,8 +25,12 @@
     forest = t8_forest_new_uniform(cmesh, scheme, 0, 0, comm)
     wrapper_B = T8code.ForestWrapper(forest)
 
+    @test length(T8code.T8CODE_OBJECT_TRACKER) == 2
+
     # Finalize the first wrapper.
     finalize(wrapper_A)
+
+    @test length(T8code.T8CODE_OBJECT_TRACKER) == 1
 
     # The second wrapper should be finalized automatically when Julia shuts down.
     # ... finalize(wrapper_B) ...

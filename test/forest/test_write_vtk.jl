7165014e87a339f5842535840416ddc2799b6faf
bbolm <benjamin.bolm@gmx.de>
2025-02-06 11:45:12 +0100

    Adapt test

--- test/forest/test_write_vtk.jl
+++ test/forest/test_write_vtk.jl
@@ -3,38 +3,43 @@
 @testset "test_write_vtk" begin
     outdir = "out_test"
     isdir(outdir) && rm(outdir, recursive = true)
-    mkdir(outdir)
+    mkpath(outdir)
+
     for eclass in T8_ECLASS_ZERO:t8_eclass(T8_ECLASS_COUNT - 1)
-        for level in 0:3
-            scheme = t8_scheme_new_default_cxx()
-            cmesh = t8_cmesh_new_hypercube(t8_eclass(eclass), comm, 0, 0, 0)
-            forest = t8_forest_new_uniform(cmesh, scheme, level, 0, comm)
+        @testset "eclass $eclass" begin
+            for level in 0:3
+                scheme = t8_scheme_new_default_cxx()
+                cmesh = t8_cmesh_new_hypercube(t8_eclass(eclass), comm, 0, 0, 0)
+                forest = t8_forest_new_uniform(cmesh, scheme, level, 0, comm)
 
-            num_elements = t8_forest_get_local_num_elements(forest)
+                num_elements = t8_forest_get_local_num_elements(forest)
 
-            data = rand(num_elements)
+                data = rand(num_elements)
 
-            vtk_data = t8_vtk_data_field_t(T8_VTK_SCALAR,
-                                           NTuple{8192, Cchar}(rpad("data_1)\0", 8192, ' ')),
-                                           pointer(data))
+                vtk_data = t8_vtk_data_field_t(T8_VTK_SCALAR,
+                                            NTuple{8192, Cchar}(rpad("data_1)\0", 8192, ' ')),
+                                            pointer(data))
 
-            # The number of user defined data fields to write.
-            num_data = 1
+                # The number of user defined data fields to write.
+                num_data = 1
 
-            # Write user defined data to vtu file.
-            write_treeid = 1
-            write_mpirank = 1
-            write_level = 1
-            write_element_id = 1
-            write_ghosts = 0
-            file = joinpath(outdir, "data_" * string(eclass) * "_" * string(level))
-            t8_forest_write_vtk_ext(forest, file, write_treeid, write_mpirank,
-                                    write_level, write_element_id, write_ghosts,
-                                    0, 0, num_data, Ref(vtk_data))
+                # Write user defined data to vtu file.
+                write_treeid = 1
+                write_mpirank = 1
+                write_level = 1
+                write_element_id = 1
+                write_ghosts = 0
+                file = joinpath(outdir, "data_" * string(eclass) * "_" * string(level))
+                t8_forest_write_vtk_ext(forest, file, write_treeid, write_mpirank,
+                                        write_level, write_element_id, write_ghosts,
+                                        0, 0, num_data, Ref(vtk_data))
+                # TODO: Add MPI barrier
+                @test isfile(file * ".pvtu")
+                @test isfile(file * "_0000.vtu")
+                # TODO: Check file * "0001.vtu" for MPI parallel run
 
-            @test isfile(file * "_0000.vtu") && isfile(file * ".pvtu")
-
-            t8_forest_unref(Ref(forest))
+                t8_forest_unref(Ref(forest))
+            end
         end
     end
 

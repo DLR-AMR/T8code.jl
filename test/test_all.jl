e76725e5dabbd15b39c68c1c0579465a7345d932
Thomas Spenke <thomas.spenke@dlr.de>
2025-05-21 07:43:11 +0200

    Deactivate cmesh case again

--- test/test_all.jl
+++ test/test_all.jl
@@ -27,9 +27,9 @@ end
     include("test_forestwrapper.jl")
 end
 
-@testset "cmesh" begin
-    include("cmesh/test_readmshfile.jl")
-end
+# @testset "cmesh" begin
+#     include("cmesh/test_readmshfile.jl")
+# end
 
 @testset "forest" begin
     include("forest/test_element_volume.jl")

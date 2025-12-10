d120ef1408869e08232289e6cc2d56bc449d55a8
Thomas Spenke <thomas.spenke@dlr.de>
2025-12-10 13:11:49 +0100

    Make each example an own test

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -3,20 +3,35 @@
 # It does not add extra sanity checks, assertions etc. to the examples,
 # but is intended to make sure the exampples run without throwing errors.
 
-include("../examples/t8_step0_helloworld.jl")
-
-include("../examples/t8_step1_coarsemesh.jl")
-
-include("../examples/t8_step2_uniform_forest.jl")
-
-include("../examples/t8_step3_adapt_forest.jl")
-
-include("../examples/t8_step4_partition_balance_ghost.jl")
-
-if !Sys.iswindows()
-  include("../examples/t8_step5_element_data.jl")
+@testset "t8_step0_helloworld" begin
+  include("../examples/t8_step0_helloworld.jl")
 end
 
-include("../examples/t8_step6_stencil.jl")
+@testset "t8_step1_coarsemesh" begin
+  include("../examples/t8_step1_coarsemesh.jl")
+end
 
-include("../examples/t8_tutorial_build_cmesh.jl")
+@testset "t8_step2_uniform_forest" begin
+  include("../examples/t8_step2_uniform_forest.jl")
+end
+
+@testset "t8_step3_adapt_forest" begin
+  include("../examples/t8_step3_adapt_forest.jl")
+end
+
+@testset "t8_step4_partition_balance_ghost" begin
+  include("../examples/t8_step4_partition_balance_ghost.jl")
+end
+
+if !Sys.iswindows()
+  @testset "t8_step5_element_data" begin
+    include("../examples/t8_step5_element_data.jl")
+  end
+end
+@testset "t8_step6_stencil" begin
+  include("../examples/t8_step6_stencil.jl")
+end
+
+@testset "t8_tutorial_build_cmesh" begin
+  include("../examples/t8_tutorial_build_cmesh.jl")
+end

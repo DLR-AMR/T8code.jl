e959a1ad967f3e884ab4b605fd08ea39b8c0a30b
Thomas Spenke <thomas.spenke@dlr.de>
2025-12-10 13:21:34 +0100

    Skip steps 5 and 6 for MacOS too

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -4,37 +4,37 @@
 # but is intended to make sure the exampples run without throwing errors.
 
 @testset "t8_step0_helloworld" begin
-  include("../examples/t8_step0_helloworld.jl")
+    include("../examples/t8_step0_helloworld.jl")
 end
 
 @testset "t8_step1_coarsemesh" begin
-  include("../examples/t8_step1_coarsemesh.jl")
+    include("../examples/t8_step1_coarsemesh.jl")
 end
 
 @testset "t8_step2_uniform_forest" begin
-  include("../examples/t8_step2_uniform_forest.jl")
+    include("../examples/t8_step2_uniform_forest.jl")
 end
 
 @testset "t8_step3_adapt_forest" begin
-  include("../examples/t8_step3_adapt_forest.jl")
+    include("../examples/t8_step3_adapt_forest.jl")
 end
 
 @testset "t8_step4_partition_balance_ghost" begin
-  include("../examples/t8_step4_partition_balance_ghost.jl")
+    include("../examples/t8_step4_partition_balance_ghost.jl")
 end
 
-if !Sys.iswindows()
-  @testset "t8_step5_element_data" begin
-    include("../examples/t8_step5_element_data.jl")
-  end
+if !Sys.iswindows() && !Sys.isapple()
+    @testset "t8_step5_element_data" begin
+        include("../examples/t8_step5_element_data.jl")
+    end
 end
 
-if !Sys.iswindows()
-  @testset "t8_step6_stencil" begin
-    include("../examples/t8_step6_stencil.jl")
-  end
+if !Sys.iswindows() && !Sys.isapple()
+    @testset "t8_step6_stencil" begin
+        include("../examples/t8_step6_stencil.jl")
+    end
 end
 
 @testset "t8_tutorial_build_cmesh" begin
-  include("../examples/t8_tutorial_build_cmesh.jl")
+    include("../examples/t8_tutorial_build_cmesh.jl")
 end

ad86589f34933871f8b94efb7e98345b9f9956ab
Benedict Geihe <bgeihe@uni-koeln.de>
2026-01-06 17:09:40 +0100

    update generator.toml and fixes.sh
    move everything to ignore to generator.toml, try to keep manual fixes
    list short

--- dev/fixes.sh
+++ dev/fixes.sh
@@ -21,36 +21,8 @@ sed -i "s/t8_forest_get_mpicomm(forest::t8_forest_t)::Cint/t8_forest_get_mpicomm
 # Remove struct t8_forest definition and replace by forward declaration
 sed -i -z 's/\nstruct t8_forest.*stats_computed::Cint\nend/\n# This struct is not supposed to be read and modified directly.\n# Besides, there is a circular dependency with `t8_forest_t`\n# leading to an error output by Julia.\nmutable struct t8_forest end/g' "${LIB_JL}"
 
+# Fix forest type
 sed -i "s/forest::Cint/forest::t8_forest_t/" "${LIB_JL}"
 
-# Remove Fortran macros
-sed -i "/INTEGER(KIND/d" "${LIB_JL}"
-
-# Remove local arrays
-sed -i "/T8_.*_VALUES/d" "${LIB_JL}"
-
-# Remove misc
-sed -i "/T8_VERSION_POINT/d" "${LIB_JL}"
-
-sed -i "/P4EST_GLOBAL_NOTICE/d" "${LIB_JL}"
-sed -i "/P4EST_NOTICE/d" "${LIB_JL}"
-
-sed -i "/_sc_const/d" "${LIB_JL}"
-sed -i "/_sc_restrict/d" "${LIB_JL}"
-
-# Remove MPI macros not available in MPI.jl
-sed -i "/sc_MPI_PACKED/d" "${LIB_JL}"
-sed -i "/sc_MPI_Pack/d" "${LIB_JL}"
-sed -i "/sc_MPI_Unpack/d" "${LIB_JL}"
-sed -i "/sc_MPI_DOUBLE_INT/d" "${LIB_JL}"
-
-sed -i "/= MPI_MODE_/d" "${LIB_JL}"
-sed -i "/= MPI_SEEK_/d" "${LIB_JL}"
-sed -i "/= MPI_ERR_/d" "${LIB_JL}"
-sed -i "/= MPI_MAX_/d" "${LIB_JL}"
-sed -i "/= MPI_Type_/d" "${LIB_JL}"
-sed -i "/= MPI_Offset/d" "${LIB_JL}"
-sed -i "/= MPI_File_/d" "${LIB_JL}"
-sed -i "/= MPI_Aint/d" "${LIB_JL}"
-
+# Rename remaining MPI macros
 sed -i "s/= MPI_/= MPI./" "${LIB_JL}"

57a99079aaa2fef1ad33f4f90eaa3f43cdd6d579
Benedict Geihe <bgeihe@uni-koeln.de>
2026-07-07 11:10:28 +0200

    Revert "try reading the platform BUFSIZ from t8_vtk_data_field_t"
    This reverts commit f231c85ab45780f984c604eabdbaa28f8e2177d3.

--- examples/t8_step6_stencil.jl
+++ examples/t8_step6_stencil.jl
@@ -339,13 +339,13 @@ function t8_step6_output_data_to_vtu(forest, element_data, prefix)
     # WARNING: This code hangs for Julia v1.8.* or older. Use at least Julia v1.9.
     vtk_data = [
         t8_vtk_data_field_t(T8_VTK_SCALAR,
-                            NTuple{T8code.T8_BUFSIZ, Cchar}(rpad("height\0", T8code.T8_BUFSIZ, ' ')),
+                            NTuple{8192, Cchar}(rpad("height\0", 8192, ' ')),
                             pointer(heights)),
         t8_vtk_data_field_t(T8_VTK_SCALAR,
-                            NTuple{T8code.T8_BUFSIZ, Cchar}(rpad("schlieren\0", T8code.T8_BUFSIZ, ' ')),
+                            NTuple{8192, Cchar}(rpad("schlieren\0", 8192, ' ')),
                             pointer(schlieren)),
         t8_vtk_data_field_t(T8_VTK_SCALAR,
-                            NTuple{T8code.T8_BUFSIZ, Cchar}(rpad("curvature\0", T8code.T8_BUFSIZ, ' ')),
+                            NTuple{8192, Cchar}(rpad("curvature\0", 8192, ' ')),
                             pointer(curvature))
     ]
 
@@ -402,8 +402,10 @@ t8_step6_exchange_ghost_data(forest, element_data)
 t8_step6_compute_stencil(forest, element_data)
 
 # Output the data to vtu files.
-t8_step6_output_data_to_vtu(forest, element_data, prefix_forest_with_data)
-t8_global_productionf(" Wrote forest and data to %s*.\n", prefix_forest_with_data)
+if !(CI_ON_WINDOWS || CI_ON_MACOS)
+    t8_step6_output_data_to_vtu(forest, element_data, prefix_forest_with_data)
+    t8_global_productionf(" Wrote forest and data to %s*.\n", prefix_forest_with_data)
+end
 
 #
 # Clean-up

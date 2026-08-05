5f9d1cc95d2fc47b8edeacbebe48c9deb983e2dc
Benedict Geihe <bgeihe@uni-koeln.de>
2026-08-05 09:31:22 +0200

    update to Clang 0.19
    remove glob
    use header detection suggested by Clang

--- dev/generator.jl
+++ dev/generator.jl
@@ -7,7 +7,6 @@ Pkg.instantiate()
 using Artifacts
 cp(joinpath(artifact"t8code", "include"), "t8code_include"; force = true)
 
-using Glob
 using Clang.Generators
 
 cd(@__DIR__)
@@ -19,12 +18,9 @@ options = load_options(joinpath(@__DIR__, "generator.toml"))
 args = get_default_args()  # Note you must call this function firstly and then append your own flags
 push!(args, "-I$include_dir")
 
-headers = [
-  glob("t8_*.h", include_dir) ; 
-  glob("**/t8_*.h", include_dir) ; 
-  glob("**/**/t8_*.h", include_dir) ;
-  # glob("**/**/**/t8_*.h", include_dir)
-]
+headers = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, ".h")]
+# there is also an experimental `detect_headers` function for auto-detecting top-level headers in the directory
+# headers = detect_headers(include_dir, args)
 
 # create context
 ctx = create_context(headers, args, options)

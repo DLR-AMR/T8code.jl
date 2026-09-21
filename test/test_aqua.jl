49b8b984268d67906fa44e864b881a8a1d8dd1ce
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-09-21 13:33:22 +0200

    add ExplicitImports.jl tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,11 +1,17 @@
 module TestAqua
 
-using Aqua
+using Aqua: Aqua
+using ExplicitImports: test_explicit_imports
 using Test
 using T8code
 
 @testset "Aqua.jl" begin
     Aqua.test_all(T8code)
+end
+
+
+@testset "ExplicitImports.jl" begin
+    test_explicit_imports(T8code)
 end
 
 end #module

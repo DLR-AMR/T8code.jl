60de65d93033300e47e4d14b1171847f800edeb7
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-09-21 13:48:57 +0200

    fix ExplicitImports.jl tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -10,7 +10,11 @@ using T8code
 end
 
 @testset "ExplicitImports.jl" begin
-    test_explicit_imports(T8code)
+    test_explicit_imports(T8code,
+                          # We use `MPI_Comm` and `MPI_File`, which are non-public
+                          all_explicit_imports_are_public = false,
+                          # We use `MPIPreferences.binary`, which is non-public
+                          all_qualified_accesses_are_public = false)
 end
 
 end #module

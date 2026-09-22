9b1dc98bd4c7f241e568e660f07bae6d9b05dccd
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2026-09-22 10:17:37 +0000

    add ExplicitImports.jl tests (#141)
    * add ExplicitImports.jl tests
    
    * format
    
    * fix ExplicitImports.jl tests

--- src/Libt8.jl
+++ src/Libt8.jl
@@ -41,7 +41,7 @@ end
 const ptrdiff_t = Cptrdiff_t
 
 # Definitions used from MPI.jl
-using MPI: MPI, MPI_Datatype, MPI_Comm, MPI_File
+using MPI: MPI, MPI_Comm, MPI_File
 
 const MPI_COMM_WORLD = MPI.COMM_WORLD
 const MPI_COMM_SELF = MPI.COMM_SELF
@@ -4999,7 +4999,7 @@ end
 Convert points in the reference space of an element to points in the reference space of the tree.
 
 ```c++
- [0,1]^\\mathrm{dim} 
+ [0,1]^\\mathrm{dim}
 ```
 
 of the point in the reference space of the element.
@@ -15996,7 +15996,7 @@ end
 Definition of an analytic geometry function. This function maps reference coordinates to physical coordinates.
 
 ```c++
- [0,1]^\\mathrm{dim} 
+ [0,1]^\\mathrm{dim}
 ```
 
 .
@@ -16017,19 +16017,19 @@ const t8_geom_analytic_fn = Ptr{Cvoid}
 Definition for the jacobian of an analytic geometry function.
 
 ```c++
- [0,1]^\\mathrm{dim} 
+ [0,1]^\\mathrm{dim}
 ```
 
 .
 
 ```c++
- \\mathrm{dim} 
+ \\mathrm{dim}
 ```
 
 to map.
 
 ```c++
- \\mathrm{dim} \\cdot 3 
+ \\mathrm{dim} \\cdot 3
 ```
 
 x *num_coords*. Indices
@@ -16041,31 +16041,31 @@ x *num_coords*. Indices
 ,
 
 ```c++
- 3 \\cdot i+1 
+ 3 \\cdot i+1
 ```
 
 ,
 
 ```c++
- 3 \\cdot i+2 
+ 3 \\cdot i+2
 ```
 
 correspond to the
 
 ```c++
- i 
+ i
 ```
 
 -th column of the jacobian (Entry
 
 ```c++
- 3 \\cdot i + j 
+ 3 \\cdot i + j
 ```
 
 is
 
 ```c++
- \\frac{\\partial f_j}{\\partial x_i} 
+ \\frac{\\partial f_j}{\\partial x_i}
 ```
 
 ).
@@ -16624,13 +16624,13 @@ const t8_dhex_t = t8_dhex
 Convert points in the reference space of a hex element to points in the reference space of the tree (level 0) embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 )
@@ -17136,7 +17136,7 @@ end
 Convert points in the reference space of a line element to points in the reference space of the tree (level 0) embedded in [0,1]^1.
 
 ```c++
- [0,1]^1 
+ [0,1]^1
 ```
 
 )
@@ -17802,7 +17802,7 @@ end
 Compute the reference coordinates of a vertex of a prism when the  tree (level 0) is embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
@@ -17826,13 +17826,13 @@ end
 Convert points in the reference space of a prism element to points in the reference space of the tree (level 0) embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 )
@@ -18650,7 +18650,7 @@ end
 Compute the reference coordinates of a vertex of a pyramid when the tree (level 0 triangle) is embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
@@ -18674,13 +18674,13 @@ end
 Convert points in the reference space of a pyramid element to points in the reference space of the tree (level 0) embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 )
@@ -18759,7 +18759,7 @@ end
 Compute the coordinates of a vertex of a tetrahedron when the  tree (level 0 tetrahedron) is embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
@@ -18783,13 +18783,13 @@ end
 Convert points in the reference space of a tet element to points in the reference space of the tree (level 0) embedded in
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 .
 
 ```c++
- [0,1]^3 
+ [0,1]^3
 ```
 
 )
@@ -19668,7 +19668,7 @@ end
 Compute the reference coordinates of a vertex of a triangle when the  tree (level 0 triangle) is embedded in
 
 ```c++
- [0,1]^2 
+ [0,1]^2
 ```
 
 .
@@ -19692,13 +19692,13 @@ end
 Convert points in the reference space of a tri element to points in the reference space of the tree (level 0) embedded in
 
 ```c++
- [0,1]^2 
+ [0,1]^2
 ```
 
 .
 
 ```c++
- [0,1]^2 
+ [0,1]^2
 ```
 
 )
--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,11 +1,20 @@
 module TestAqua
 
-using Aqua
+using Aqua: Aqua
+using ExplicitImports: test_explicit_imports
 using Test
 using T8code
 
 @testset "Aqua.jl" begin
     Aqua.test_all(T8code)
+end
+
+@testset "ExplicitImports.jl" begin
+    test_explicit_imports(T8code,
+                          # We use `MPI_Comm` and `MPI_File`, which are non-public
+                          all_explicit_imports_are_public = false,
+                          # We use `MPIPreferences.binary`, which is non-public
+                          all_qualified_accesses_are_public = false)
 end
 
 end #module

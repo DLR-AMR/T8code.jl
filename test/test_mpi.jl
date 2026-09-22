8d06b57a5dc5c20e508446f02879c45655b387f9
Benedict Geihe <bgeihe@uni-koeln.de>
2026-09-22 12:32:15 +0200

    based on loaded_modules

--- test/test_mpi.jl
+++ test/test_mpi.jl
@@ -1,5 +1,6 @@
 @testset "Conditional loading of t8code_jll" begin
-    @test (JULIA_MPI_PROVIDER == "SYSTEM_MPI") != (:t8code_jll in names(T8code; imported=true))
+    t8code_jll_loaded = any(m -> nameof(m) === :t8code_jll, values(Base.loaded_modules))
+    @test (JULIA_MPI_PROVIDER == "SYSTEM_MPI") != t8code_jll_loaded
 end
 
 @testset "T8code.uses_mpi" begin

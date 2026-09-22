18949e73325e4f71d44cb32706fc4b644c274a74
Benedict Geihe <bgeihe@uni-koeln.de>
2026-09-22 12:09:22 +0200

    skip aqua test when running with system MPI

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -4,8 +4,14 @@ using Aqua
 using Test
 using T8code
 
-@testset "Aqua.jl" begin
-    Aqua.test_all(T8code)
+# When run in CI, check if we are running with system MPI or Julia artifacts
+JULIA_MPI_PROVIDER = get(ENV, "JULIA_MPI_PROVIDER", "JLL_MPI")
+
+# in case we are running with system MPI t8code_jll will not be loaded
+if JULIA_MPI_PROVIDER == "JLL_MPI"
+    @testset "Aqua.jl" begin
+        Aqua.test_all(T8code)
+    end
 end
 
 end #module

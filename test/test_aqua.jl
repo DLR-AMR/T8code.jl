3057762ff43ff908c79ef92159bf46042f6a5a3a
Benedict Geihe <bgeihe@uni-koeln.de>
2026-09-23 15:12:34 +0200

    conditionally disable persistant tasks check

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -12,11 +12,15 @@ JULIA_MPI_PROVIDER = get(ENV, "JULIA_MPI_PROVIDER", "JLL_MPI")
     # in case we are running with system MPI t8code_jll will not be loaded
     if JULIA_MPI_PROVIDER == "SYSTEM_MPI"
         stale_deps_ignore = (ignore = [:t8code_jll],)
+        test_persistent_tasks = false
     else
         stale_deps_ignore = ()
+        test_persistent_tasks = true
     end
 
-    Aqua.test_all(T8code; stale_deps = stale_deps_ignore)
+    Aqua.test_all(T8code;
+                  test_persistent_tasks = test_persistent_tasks,
+                  stale_deps = stale_deps_ignore)
 end
 
 @testset "ExplicitImports.jl" begin

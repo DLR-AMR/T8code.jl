ea3152d96c3ede9ee9d2827dce4c20cee67193f1
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2026-09-24 09:24:47 +0000

    Load t8code_jll only if preferences say so also on prologue.jl (#144)

--- dev/prologue.jl
+++ dev/prologue.jl
@@ -1,7 +1,10 @@
-using t8code_jll: t8code_jll
-export t8code_jll
-
 using ..T8code: _PREFERENCE_LIBT8, _PREFERENCE_LIBP4EST, _PREFERENCE_LIBSC
+
+@static if _PREFERENCE_LIBT8 == "t8code_jll" || _PREFERENCE_LIBP4EST == "t8code_jll" ||
+           _PREFERENCE_LIBSC == "t8code_jll"
+    using t8code_jll: t8code_jll
+end
+
 using MPIPreferences: MPIPreferences
 
 @static if _PREFERENCE_LIBT8 == "t8code_jll" && MPIPreferences.binary == "system"

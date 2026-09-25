78bd04791a26914c68d808af2d539cadf9083fc8
Benedict <135045760+benegee@users.noreply.github.com>
2026-09-25 08:55:07 +0200

    CI test with system MPI and local t8code installation (#143)
    * new CI test with system MPI and local t8code
    
    * add NEWS.md
    
    * revert renaming
    
    * add julia compat bound to test
    
    * typo
    
    using other list syntax now
    
    * try to dev T8code
    
    * relative to project
    
    * explicit arch
    
    * use dirname
    
    * wrong path?
    
    * dev first?
    
    * sequence
    
    * split configure steps
    
    * OpenMPI 4 workaround
    
    * workaround needed in tests as well
    
    * add MPI testing file
    
    * only load t8code_jll if preferences say so
    
    * remove workaround
    
    * fix
    
    * OpenMPI_jll v4 again
    
    * check module T8code
    
    * skip aqua test when running with system MPI
    
    * based on loaded_modules
    
    * lower bound reported by Downgrade
    
    * need absolute path
    
    * check again
    
    * Revert "only load t8code_jll if preferences say so"
    
    This reverts commit c4adcf86a96f790787f14e55f6fca3345f4bafa2.
    
    * free T8code package again afterwards
    
    * only load t8code_jll if preferences say so
    
    * remove build step
    
    * conditional ignore for Aqua test
    
    * fmt
    
    * pass Tuple to stale_deps
    
    * correct ignore
    
    * conditionally disable persistant tasks check
    
    * can we always use Aqua 0.8?
    
    * do not use T8code to set the preferences
    
    * use root project
    
    * abspath
    
    * add a precompile step
    
    * separate handling of julia nightly
    
    * duh
    
    * prevent precompiling again
    
    * flags as in runtest
    
    * even more args

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -5,8 +5,22 @@ using ExplicitImports: test_explicit_imports
 using Test
 using T8code
 
+# When run in CI, check if we are running with system MPI or Julia artifacts
+JULIA_MPI_PROVIDER = get(ENV, "JULIA_MPI_PROVIDER", "JLL_MPI")
+
 @testset "Aqua.jl" begin
-    Aqua.test_all(T8code)
+    # in case we are running with system MPI t8code_jll will not be loaded
+    if JULIA_MPI_PROVIDER == "SYSTEM_MPI"
+        stale_deps_ignore = (ignore = [:t8code_jll],)
+        test_persistent_tasks = false
+    else
+        stale_deps_ignore = ()
+        test_persistent_tasks = true
+    end
+
+    Aqua.test_all(T8code;
+                  persistent_tasks = test_persistent_tasks,
+                  stale_deps = stale_deps_ignore)
 end
 
 @testset "ExplicitImports.jl" begin
